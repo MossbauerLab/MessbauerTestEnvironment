e1a9e2372f3a8abef08b1074fd8a1eed1821e8e7
Unknown <um.nix.user@gmail.com>
2017-08-30 13:29:37 +0400

    Testbench on generator, generator works in first regime

--- messbaue_test_environment/src/messbauer_generator.v
+++ messbaue_test_environment/src/messbauer_generator.v
@@ -29,17 +29,17 @@
 	  parameter GCLK_PERIOD = 20,                                                                    // nanoseconds
      parameter START_DURATION = 50,                                                                 // number of GCLK periods (aclk port), 1 clock is 20ns
      parameter CHANNEL_NUMBER = 512,                                                                // is a degree of 2 i.e. 128, 256, 512, 1024 and others smaller than 4096 ! 
-     parameter CHANNEL_DURATION = (16 * (`MAX_CHANNEL_NUMBER / CHANNEL_NUMBER)) * 1000 / GCLK_PERIOD, // channel duration in clock periods
+     parameter CHANNEL_DURATION = (16 * (`MAX_CHANNEL_NUMBER / CHANNEL_NUMBER)) * 1000 / (2 *GCLK_PERIOD), // channel duration in clock periods
 	  parameter CHANNEL_TYPE = `CHANNEL_AFTER_MEASURE                                                // options are 1) START_AND_CHANNEL_SYNC or 2) CHANNEL_AFTER_MEASURE
  )
  (
      input wire aclk,
-	  input wire areset,
+	  input wire areset_n,
 	  output reg start,
 	  output reg channel
  );
  
- localparam CHANNEL_GUARD_DURATION = CHANNEL_DURATION - (1000 / GCLK_PERIOD); // for switch before 1 us
+ localparam CHANNEL_GUARD_DURATION = CHANNEL_DURATION - 4 * (1000 / GCLK_PERIOD); // for switch before 4 us
  localparam CHANNEL_MEANDR_GUARD_DURATION = CHANNEL_DURATION / 2;
  localparam START_HIGH_PHASE_DURATION = 15464 * (1000 / GCLK_PERIOD);
  
@@ -54,7 +54,7 @@
   
  always @(posedge aclk)
  begin
-     if(~areset)
+     if(~areset_n)
 	  begin
 	      start <= 1'b1;
 		   channel <= 1'b1;
